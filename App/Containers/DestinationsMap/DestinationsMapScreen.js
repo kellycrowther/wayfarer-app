cde41d4ceed3317381e0cb201f820e9c949e8d6c
Kelly Crowther <crowther.kelly1@gmail.com>
2019-06-03 12:22:40 -0700

    hook up mapbox to work with ios build; downloaded mapbox.framework sdk from mapbox directly, added to node_modules and followed install directions noramlly; added marker and html to render the map and markers

--- App/Containers/DestinationsMap/DestinationsMapScreen.js
+++ App/Containers/DestinationsMap/DestinationsMapScreen.js
@@ -1,21 +1,92 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image } from 'react-native'
 import { connect } from 'react-redux'
 import Style from './DestinationsMapScreenStyle'
+import MapboxGL from '@react-native-mapbox-gl/maps'
+
+MapboxGL.setAccessToken(
+  'pk.eyJ1Ijoia2VsbHljcm93dGhlciIsImEiOiJjandmbWN0emIweDNmNDRrZHV3YzV0b3BqIn0.S-VaWf5_L6ZFUFWqZjglBQ'
+)
 
 class DestinationsMapScreen extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      coordinates: [
+        [-73.98330688476561, 40.76975180901395],
+        [-73.96682739257812, 40.761560925502806],
+        [-74.00751113891602, 40.746346606483826],
+        [-73.95343780517578, 40.7849607714286],
+        [-73.99017333984375, 40.71135347314246],
+        [-73.98880004882812, 40.758960433915284],
+        [-73.96064758300781, 40.718379593199494],
+        [-73.95172119140624, 40.82731951134558],
+        [-73.9829635620117, 40.769101775774935],
+        [-73.9822769165039, 40.76273111352534],
+        [-73.98571014404297, 40.748947591479705],
+      ],
+    }
   }
   componentDidMount() {
     console.info('DestinationsMapScreen->componentDidMount', this.props)
+    MapboxGL.setTelemetryEnabled(false)
+  }
+
+  renderAnnotation(counter) {
+    const id = `pointAnnotation${counter}`
+    const coordinate = this.state.coordinates[counter]
+    const title = `Longitude: ${this.state.coordinates[counter][0]} Latitude: ${
+      this.state.coordinates[counter][1]
+    }`
+
+    return (
+      <MapboxGL.PointAnnotation
+        key={id}
+        id={id}
+        title={title}
+        coordinate={coordinate}
+        onSelected={console.info('Clicked Marker!')}
+      >
+        <Image
+          source={require('App/Images/marker.png')}
+          style={{
+            flex: 1,
+            resizeMode: 'contain',
+            width: 25,
+            height: 25,
+          }}
+        />
+      </MapboxGL.PointAnnotation>
+    )
+  }
+
+  renderAnnotations() {
+    const items = []
+
+    for (let i = 0; i < this.state.coordinates.length; i++) {
+      items.push(this.renderAnnotation(i))
+    }
+
+    return items
   }
 
   render() {
     return (
-      <View style={Style.container}>
-        <Text>Map is working</Text>
+      <View style={Style.page}>
+        <View style={Style.container}>
+          <MapboxGL.MapView
+            // eslint-disable-next-line no-return-assign
+            ref={(c) => (this._map = c)}
+            style={{ flex: 1 }}
+            zoomLevel={11}
+            showUserLocation={true}
+            userTrackingMode={1}
+            centerCoordinate={this.state.coordinates[0]}
+          >
+            {this.renderAnnotations()}
+          </MapboxGL.MapView>
+        </View>
       </View>
     )
   }
--- App/Containers/DestinationsMap/DestinationsMapScreenStyle.js
+++ App/Containers/DestinationsMap/DestinationsMapScreenStyle.js
@@ -1,60 +1,19 @@
 /* eslint-disable react-native/no-color-literals */
 import { StyleSheet } from 'react-native'
-import Fonts from 'App/Theme/Fonts'
-import ApplicationStyles from 'App/Theme/ApplicationStyles'
 
 export default StyleSheet.create({
   container: {
-    ...ApplicationStyles.screen.container,
+    backgroundColor: 'tomato',
+    height: 300,
+    width: 300,
+  },
+  map: {
+    flex: 1,
+  },
+  page: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
     flex: 1,
     justifyContent: 'center',
-    margin: 30,
-  },
-  error: {
-    ...Fonts.style.normal,
-    color: 'red',
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  inputContainer: {
-    margin: 10,
-  },
-  instructions: {
-    ...Fonts.style.normal,
-    fontStyle: 'italic',
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  loading: {
-    ...Fonts.style.normal,
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  logo: {
-    height: '100%',
-    width: '100%',
-  },
-  logoContainer: {
-    height: 300,
-    marginBottom: 25,
-    width: '100%',
-  },
-  result: {
-    ...Fonts.style.normal,
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  text: {
-    ...Fonts.style.normal,
-    marginBottom: 5,
-    textAlign: 'center',
-  },
-  textInput: {
-    marginLeft: 5,
-  },
-  title: {
-    ...Fonts.style.h2,
-    marginBottom: 10,
-    textAlign: 'center',
   },
 })
